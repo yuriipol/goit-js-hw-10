89b69fbaca1bcb69be9e5d3c7e58018da1de7eb4
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-15 16:58:16 +0300

    change task

--- src/index.js
+++ src/index.js
@@ -1,50 +1,78 @@
 import './css/styles.css';
 import Debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
 
 const inputFill = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 inputFill.addEventListener('input', Debounce(onInputSerch, DEBOUNCE_DELAY));
 
 function onInputSerch(event) {
   event.preventDefault();
+
   console.log(event.target.value);
+
+  const inputValue = event.target.value;
+  const name = inputValue.trim();
+  if ((name.length = '')) {
+    return Notiflix.Notify.info('This field should not be empty');
+  }
+  clearContent();
+
+  fetchCountries(name)
+    .then(serchCountry => renderContent(serchCountry))
+    .catch(error =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
 }
 
-const name = 'U';
-fetchCountries(name)
-  .then(data => data.json())
-  .then(countrys => countrys.filter(country => country.name.includes(name)))
-  .then(serchCountry => console.log(serchCountry));
-
-function renderCountry(countrys) {
+function renderCountryList(countrys) {
   const markup = countrys
-    .map(country => {
-      return `<li>
-            <svg width="200" height="200"
-            <image href="${countrys.flags.svg}" height="200" width="200"/>
-            </svg>
-          <p><b>Country</b>: ${countrys.name}</p>
+    .map(({ name, flags: { svg } } = {}) => {
+      return `<li class="country">
+            
+            <img src = "${svg}" height="200" width="200"/>
+            
+          <p><b>Country</b>: ${name}</p>
         </li>`;
     })
     .join('');
   countryList.innerHTML = markup;
 }
 
-// function makeGallaryCountrys({
-//   name,
-//   capital,
-//   population,
-//   flags: { svg },
-//   languages,
-// } = {}) {
-//   return `<li>
-//             <svg width="200" height="200"
-//             <image href="${svg}" height="200" width="200"/>
-//             </svg>
-//           <p><b>Country</b>: ${name}</p>
-//         </li>`;
-// }
-// const gallaryCountrys = serchCountry.map(makeGallaryCountrys);
-// countryList.insertAdjacentHTML('afterbegin', gallaryCountrys.join(''));
+function renderContent(countrys) {
+  if (countrys.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  }
+  if (countrys.length > 1) {
+    renderCountryList(countrys);
+    return;
+  }
+  renderCountry(countrys[0]);
+}
+
+function renderCountry({ name, flags: { svg } } = {}) {
+  const countyContent = `  
+          <img src = "${svg}" height="200" width="200"/>        
+          <p><b>Country</b>: ${name}</p>
+          <p><b>Country</b>: ${name}</p>
+          <p><b>Country</b>: ${name}</p>`;
+
+  countryInfo.innerHTML = countyContent;
+}
+
+function clearContent() {
+  if (countryList.children.length > 0) {
+    countryList.innerHTML = '';
+  }
+
+  if (countryInfo.children.length > 0) {
+    countryInfo.innerHTML = '';
+  }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,12 @@
 export const fetchCountries = name => {
   const countryPromise = fetch(
-    'https://restcountries.com/v2/all?fields=name,capital,population,flags,languages'
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
   );
-  return countryPromise;
+
+  return countryPromise.then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
